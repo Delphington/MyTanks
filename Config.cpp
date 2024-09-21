afcd1ec11935dec3a0fc708652271b2ffe437030
Delphington <Delphington@gmail.com>
2024-09-21 22:46:33 +0300

    MVP

---
+++ Config.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Professional on 21.09.2024.
+//
+#include <string>
+
+using namespace std;
+
+class Config{
+public:
+    string nameGame = "MyTanks" ;
+    Config(){};
+};
+// Инициализация статической переменной
